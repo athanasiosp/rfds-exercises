319994d0580b96d5a29bccc05fb9b59d459285c8
apliousis <tpliousis@gmail.com>
2020-03-15 22:32:40 +0200

    Created a function to estimate pi from a random uniform distribution between 0 and 1. Run a simulation and a visualization of the convergence to pi's true value.

---
+++ estimating_pi.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+
+
+estimate_pi <- function(n){
+    
+    x = runif(n, 0, 1)
+    y = runif(n, 0, 1)
+    distance = x**2 + y**2
+    
+    positions <- ifelse(test = distance<=1, yes = 1, no = 0)
+    inside_circle <- length(which(positions == 1))
+    
+    return(4*inside_circle / n)
+    
+}
+
+
+xp <- vector(mode = "integer", length = 0)
+pi_points <- vector(mode = "numeric", length = 0)
+
+for (i in seq(100,10000,100)) {
+    
+    xp <- append(xp, i)
+    
+    pi_points <- append(pi_points, estimate_pi(i))
+    
+    data <- tibble(xp, pi_points)
+}
+
+
+data %>% ggplot(aes(x = xp, y =  pi_points)) + 
+    geom_point(color = "blue") + 
+    geom_line(color = "blue") + 
+    geom_hline(aes(yintercept = 3.1415), color = "red")
+
+
+set.seed(1234)
+start_time <- proc.time()
+estimate_pi(10000000)
+proc.time()-start_time
+
+
+set.seed(1234)
+tictoc::tic("pi")
+estimate_pi(10000000)
+tictoc::toc()
